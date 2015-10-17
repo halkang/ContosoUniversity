e239ea260a57f61d25d9b13579f7a61ecc7e2fc5
halkang <dearlyhal@gmail.com>
2015-10-17 10:27:28 +0800

    EntityFramework.6.1.1 上課

---
+++ ContosoUniversity1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new ContosoUniversityEntities())
+            {
+                var data = from p in db.Course
+                           where p.Title.Contains("Git")
+                           select p;
+ 
+                foreach (var item in data)
+                {
+                    Console.WriteLine(item.CourseID + "\t" + item.Title);
+                }
+            }
+        }
+    }
+}
