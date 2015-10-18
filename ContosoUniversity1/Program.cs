be668dc4112b9d324ad428661e0294b21693b484
halkang <dearlyhal@gmail.com>
2015-10-18 11:09:32 +0800

    day2 11:09

--- ContosoUniversity1/Program.cs
+++ ContosoUniversity1/Program.cs
@@ -10,6 +10,33 @@ namespace ContosoUniversity1
     {
         static void Main(string[] args)
         {
+            Course c;
+            using (var db =new ContosoUniversityEntities())
+            {
+                c = db.Course.Find(1);
+                Console.WriteLine(c.Credits);
+                c.Credits=20;
+                Console.WriteLine(db.Entry(c).State);
+            }
+            using (var db = new ContosoUniversityEntities())
+            {
+                Console.WriteLine(db.Entry(c).State);
+                //db.Course.Attach(c);
+                db.Entry(c).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+               
+                Console.WriteLine(c.Credits);
+            }
+
+
+
+
+
+
+        }
+
+        private static void d1()
+        {
             using (var db = new ContosoUniversityEntities())
             {
                 //var data = from p in db.Course
@@ -21,49 +48,63 @@ namespace ContosoUniversity1
                 //    Console.WriteLine(item.CourseID + "\t" + item.Title);
                 //}
 
-                var Departments = db.Department.Include("Course");
-                foreach (var dept in Departments)
-                {
-                    Console.WriteLine("#" + dept.Name);
-                    foreach (var item in dept.Course)
-                    {
-                        Console.WriteLine("#############" + item.Title);
-                    }
+                //                var Departments = db.Department.Include("Course");
+                //                foreach (var dept in Departments)
+                //                {
+                //                    Console.WriteLine("#" + dept.Name);
+                //                    foreach (var item in dept.Course)
+                //                    {
+                //                        Console.WriteLine("#############" + item.Title);
+                //                    }
 
-                }
-                foreach (var c in db.Course)
-                {
-                    //c.ModifyedON = DateTime.Now;
+                //                }
+                //                foreach (var c in db.Course)
+                //                {
+                //                    //c.ModifyedON = DateTime.Now;
 
-                }
-                var c1 = new Course()
-                {
-                    ModifyedON = DateTime.Now
-                    ,
-                    DepartmentID = 1
-                    ,
-                    Title = "test"
-
-
-
-                };
-                //db.Course.Add(c1);
-
-                //db.SaveChanges();
+                //                }
+                //                var c1 = new Course()
+                //                {
+                //                    ModifyedON = DateTime.Now
+                //                    ,
+                //                    DepartmentID = 1
+                //                    ,
+                //                    Title = "test"
 
 
-                // var c2 = db.Course.Find(7);
 
-                // c2.Instructor.Add(db.Person.Find(5));
-                //db.SaveChanges();
-                var dddd =db.Database.SqlQuery<Course2> (@"select d.Name as deptName,c.Title from [dbo].[Department]  d 
-inner join Course  c
-on d.DepartmentID =c.DepartmentID");
+                //                };
+                //                //db.Course.Add(c1);
 
-                foreach (var item in dddd)
-                {
-                    Console.WriteLine("aaaa" + item.deptName); 
-                }
+                //                //db.SaveChanges();
+
+
+                //                // var c2 = db.Course.Find(7);
+
+                //                // c2.Instructor.Add(db.Person.Find(5));
+                //                //db.SaveChanges();
+                //                var dddd = db.Database.SqlQuery<Course2>(@"select d.Name as deptName,c.Title from [dbo].[Department]  d 
+                //                inner join Course  c
+                //                on d.DepartmentID =c.DepartmentID");
+
+                //                foreach (var item in dddd)
+                //                {
+                //                    Console.WriteLine("aaaa" + item.deptName);
+                //                }
+                var cc = db.Course.Find(1);
+                //cc.Credits = 2;
+
+                //cc.Credits=2;
+                //var ce = db.Entry(cc);
+
+                //if (ce.State == System.Data.Entity.EntityState.Modified) { 
+                //Console.WriteLine("Changed"+cc.Credits);
+                //Console.WriteLine("Ori"+ce.OriginalValues.GetValue<int>("Credits"));
+                //Console.WriteLine("");
+
+                //}
+
+                db.SaveChanges();
 
 
             }
